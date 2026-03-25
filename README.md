837224d34a9d14771f0cf0cca197f910f5d7eb83
ShawnLJW <contact@shawnljw.com>
2026-03-25 11:56:02 +0800

    create base python bot

---
+++ README.md
@@ -0,0 +1,19 @@
+# Parade State Bot
+
+I am tired of sending parade state, time to automate it.
+
+## Getting started
+
+### Prerequisites
+
+- [uv](https://docs.astral.sh/uv)
+
+### Starting the bot
+
+Create an env file with the telegram bot's token:
+
+Then run:
+
+```bash
+uv run --env-file=.env main.py
+```
---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please talk to me!",
+    )
+
+
+if __name__ == "__main__":
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    assert BOT_TOKEN, "No token in environment variables"
+
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
