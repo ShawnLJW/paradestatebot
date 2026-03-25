31d21455257a6203b7e10d3231fc15846e6e674e
ShawnLJW <contact@shawnljw.com>
2026-03-25 15:58:17 +0800

    persist jobs will sqlite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sqlite3
 from datetime import date, time
 from zoneinfo import ZoneInfo
 
@@ -33,18 +34,35 @@ async def send_parade_state_command(update: Update, context: ContextTypes.DEFAUL
     await send_parade_state(context, chat_id)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    _ = context.job_queue.run_daily(
+def schedule_job(chat_id: int, job_queue):
+    _ = job_queue.run_daily(
         send_parade_state_job,
         time(hour=8, minute=0, second=0, tzinfo=ZoneInfo("Asia/Singapore")),
         days=(1, 2, 3, 4, 5),
         chat_id=chat_id,
     )
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    schedule_job(chat_id, context.job_queue)
+    with sqlite3.connect("bot.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO jobs VALUES (?)", (str(chat_id),))
     _ = await context.bot.send_message(
         chat_id=chat_id,
         text="Parade state is scheduled at 8am",
     )
+
+
+def load_jobs(job_queue):
+    with sqlite3.connect("bot.db") as connection:
+        cursor = connection.cursor()
+        _ = cursor.execute("CREATE TABLE IF NOT EXISTS jobs (chat_id INTEGER)")
+        rows = cursor.execute("SELECT chat_id FROM jobs")
+        chat_ids = [row[0] for row in rows.fetchall()]
+    for chat_id in chat_ids:
+        schedule_job(chat_id, job_queue)
 
 
 if __name__ == "__main__":
@@ -52,6 +70,7 @@ if __name__ == "__main__":
     assert BOT_TOKEN, "No token in environment variables"
 
     application = ApplicationBuilder().token(BOT_TOKEN).build()
+    load_jobs(application.job_queue)
 
     start_handler = CommandHandler("start", start)
     send_handler = CommandHandler("send", send_parade_state_command)
